16a4ef6784461de28e5b087dca63f5d6063f3c1d
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2026-03-10 23:34:04 -0300

    fix: Add validation to category select when creating product

--- Models/CategoryViewModel.cs
+++ Models/CategoryViewModel.cs
@@ -4,6 +4,7 @@ namespace eCommerceMotoRepuestos.Models;
 
 public class CategoryViewModel
 {
+    [Required(ErrorMessage = "Seleccionar una categoría.")]
     public int CategoryId { get; set; }
     [Required (ErrorMessage = "Indicar un nombre.")]
     public string Name { get; set; }

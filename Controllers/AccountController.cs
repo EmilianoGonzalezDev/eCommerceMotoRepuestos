f1fbf54a74a9da8cdd89f67bc6c6231eb57cc567
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2026-04-15 01:26:30 -0300

    feat: Move products from session cart to persisted cart when user logs in

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceMotoRepuestos.Entities;
 using eCommerceMotoRepuestos.Models;
 using eCommerceMotoRepuestos.Services;
+using eCommerceMotoRepuestos.Utilities;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +10,10 @@ using System.Security.Claims;
 
 namespace eCommerceMotoRepuestos.Controllers;
 
-public class AccountController(UserService _userService) : Controller
+public class AccountController(
+    UserService _userService,
+    CartService _cartService,
+    ProductService _productService) : Controller
 {
     public IActionResult Login()
     {
@@ -46,6 +50,8 @@ public class AccountController(UserService _userService) : Controller
                 new ClaimsPrincipal(claimsIdentity),
                 new AuthenticationProperties() { AllowRefresh = true }
                 );
+
+            await MergeSessionCartAsync(found.UserId);
 
             return RedirectToAction("Index", "Home");
         }
@@ -85,6 +91,51 @@ public class AccountController(UserService _userService) : Controller
     {
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         return RedirectToAction("Index", "Home");
+    }
+
+    private async Task MergeSessionCartAsync(int userId)
+    {
+        var sessionCart = HttpContext.Session.Get<List<CartItemViewModel>>("Cart") ?? new List<CartItemViewModel>();
+        
+        if (!sessionCart.Any())
+        {
+            return;
+        }
+
+        foreach (var sessionItem in sessionCart)
+        {
+            var product = await _productService.GetByIdAsync(sessionItem.ProductId);
+            
+            if (product == null)
+            {
+                continue;
+            }
+
+            var currentQuantity = await _cartService.GetQuantityByUserAndProductAsync(userId, sessionItem.ProductId);
+            var totalQuantity = currentQuantity + sessionItem.Quantity;
+            var adjustedQuantity = sessionItem.Quantity;
+
+            if (totalQuantity > product.Stock)
+            {
+                adjustedQuantity = product.Stock - currentQuantity;
+                if (adjustedQuantity <= 0)
+                {
+                    continue;
+                }
+            }
+
+            var productViewModel = new ProductViewModel
+            {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Price = product.Price,
+                ImageName = product.ImageName
+            };
+
+            await _cartService.AddOrIncrementAsync(userId, productViewModel, adjustedQuantity);
+        }
+
+        HttpContext.Session.Remove("Cart");
     }
 
 }
